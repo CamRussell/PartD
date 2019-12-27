3cd09785d6dc7df2096183163965f7707e2bb7f2
CamRussell <cameraha@gmail.com>
2019-12-27 19:20:30 +1300

    Favourite mode of transport line

--- PartD/src/PartDText.java
+++ PartD/src/PartDText.java
@@ -4,5 +4,6 @@ public class PartDText {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Cam");
 		System.out.println("My favourite colour is purple");
+		System.out.println("My favourite mode of transport is bicycle");
 	}
 }
