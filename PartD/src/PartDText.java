36e96b61b110250e1c09efd75a7896f65e096397
CamRussell <cameraha@gmail.com>
2019-12-27 19:21:18 +1300

    Line adding my favourite day of the week

--- PartD/src/PartDText.java
+++ PartD/src/PartDText.java
@@ -5,5 +5,6 @@ public class PartDText {
 		System.out.println("Hi, my name is Cam");
 		System.out.println("My favourite colour is purple");
 		System.out.println("My favourite mode of transport is bicycle");
+		System.out.println("The best day of the week is Saturday");
 	}
 }
